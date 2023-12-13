3c84cc0ab39abbdadd9065e4764b686b1dfece7e
sumitbadri2000 <sumitbadri2000@gmail.com>
2023-12-13 19:27:47 +0530

    first commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const { connection } = require("./db.js");
-
+// t
 const app = express();
 const cors = require("cors");
 const { NotesRouter } = require("./routes/notes.route.js");
